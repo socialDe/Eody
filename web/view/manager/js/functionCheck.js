cf29749ed6b0d31cf068880d254de385bc2bd66f
cijbest <cijbest@gmail.com>
2020-09-20 23:53:52 +0900

    Update css in register and aregister & Fix functionCheck.js

--- web/view/manager/js/functionCheck.js
+++ web/view/manager/js/functionCheck.js
@@ -21,9 +21,9 @@ $(document).ready(function(){
         // 비밀번호 확인
         $('#pwd_check').focusout(function(){
                 if ($('#manager_pwd').val() == $('#pwd_check').val()) { 
-                $('#check_text').text("비밀번호가 일치합니다!");
+                $('#apwd_check_result').text("비밀번호가 일치합니다!");
         } else{
-                $('#check_text').text("비밀번호가 다릅니다!");
+                $('#apwd_check_result').text("비밀번호가 다릅니다!");
         } 
         });
         
--- web/view/searcher/js/functionCheck.js
+++ web/view/searcher/js/functionCheck.js
@@ -1,12 +1,29 @@
 $(document).ready(function(){
                 
+		// 아이디 중복확인
+        $('#aid_check_btn').click(function(){
+                var mid = $('input[name="searcher_id"]').val();
+                
+                $.ajax({
+              url:'idcheckimpl.mc',
+                  data: {'id': mid },
+              success: function(data){
+                                if(data == 1){
+                                        $('#id_check_result').text('이미 사용 중인 아이디입니다.');
+                                } else {
+                                        $('#id_check_result').text('사용 가능한 아이디입니다.');
+                                }
+                  },
+                });
+                
+        });
         
         // 비밀번호 확인
         $('#pwd_check').focusout(function(){
                 if ($('#searcher_pwd').val() == $('#pwd_check').val()) { 
-                $('#check_text').text("비밀번호가 일치합니다!");
+                $('#pwd_check_result').text("비밀번호가 일치합니다!");
         } else{
-                $('#check_text').text("비밀번호가 다릅니다!");
+                $('#pwd_check_result').text("비밀번호가 다릅니다!");
         } 
         });
         
