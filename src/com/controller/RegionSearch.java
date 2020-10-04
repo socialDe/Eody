5d402485136fcffff2292a233d272e4a6f88644f
socialDe <jaeuk9407@gmail.com>
2020-10-04 22:36:36 +0900

    Distance First Search Error Resolve

--- src/com/controller/RegionSearch.java
+++ src/com/controller/RegionSearch.java
@@ -1,7 +1,7 @@
 package com.controller;
 
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Collections;
 
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
@@ -34,6 +34,39 @@ public class RegionSearch {
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
+			
+			
+			//입력받은 출발지의 최대, 최소 경도 좌표
+			double maxLat=0.0, minLat=9999.9;
+			//입력받은 출발지의 최대, 최소 위도 좌표
+			double maxLng=0.0, minLng=9999.9;
+			for(int i=0; i<keys.length; i++) {
+				HotPlaceVO hotKey = new HotPlaceVO();
+				try {
+					hotKey = biz.get1(keys[i]);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				if(maxLat < Double.parseDouble(hotKey.getH_lat())) {
+					maxLat = Double.parseDouble(hotKey.getH_lat());
+				}
+				if(minLat > Double.parseDouble(hotKey.getH_lat())) {
+					minLat = Double.parseDouble(hotKey.getH_lat());
+				}
+				if(maxLng < Double.parseDouble(hotKey.getH_lng())) {
+					maxLat = Double.parseDouble(hotKey.getH_lng());
+				}
+				if(minLng > Double.parseDouble(hotKey.getH_lng())) {
+					minLat = Double.parseDouble(hotKey.getH_lng());
+				}
+			}
+			
+			//출발지와 거리 계산에 불필요한 rawdata를 제거한 ArrayList
+			ArrayList<HotPlaceVO> filteredData = new ArrayList<>();
+			for(HotPlaceVO hp : rawdata) {
+				hp.getH_lat();
+				hp.getH_lng();
+			}
 			//rawdata, 출발지간 거리를 담아두기 위한 2차원 배열 
 	        double[][] dists = new double[keys.length][rawdata.size()];
 
@@ -57,7 +90,7 @@ public class RegionSearch {
 	        		//입력 받은 위치의 lat 입력 
 	        		try {
 						lat_key = Double.parseDouble(biz.get1(keys[i]).getH_lat());
-	//					System.out.println("lat_key: "+lat_key+"입력 완료.");
+						//System.out.println("lat_key: "+lat_key+"입력 완료.");
 					} catch (NumberFormatException e) {
 						e.printStackTrace();
 					} catch (Exception e) {
@@ -93,6 +126,7 @@ public class RegionSearch {
 		            dists[i][j] = dist;
 	        	}
 	        }
+	        //거리 결과 값이 NaN값 나오는 오류(자기 위치에서 자기 위치 거리 계산 케이스) 0으로 처리
 	        for(int j=0; j<rawdata.size();j++) {
 	        	for(int i=0;i<keys.length;i++) {
 	        		if(Double.isNaN(dists[i][j])) {
@@ -101,7 +135,7 @@ public class RegionSearch {
 	        	}
 	        }
 	        
-	
+	        //dist 배열(출발지별 rawdata 거리) 입력 값 확인 
 	        for(int i=0;i<keys.length;i++) {
 	        	for(int j=0;j<rawdata.size(); j++) {
 	        		System.out.print(dists[i][j]+ " ");
@@ -110,8 +144,8 @@ public class RegionSearch {
 	        }
 	        
 	    	//rawdata별 각 출발지와 거리 계산
-	        DistanceVO[] distances = new DistanceVO[rawdata.size()];
-	        //ArrayList<DistanceVO> distances = null;
+	        //DistanceVO[] distances = new DistanceVO[rawdata.size()];
+	        ArrayList<DistanceVO> distances = new ArrayList<>();
 	        
 	        double[] sumarr = new double[rawdata.size()];
 	        
@@ -121,19 +155,22 @@ public class RegionSearch {
 	    		for(int j=0; j<keys.length;j++) {
 	    			sum += dists[j][i];
 	    		}
-	    		distances[i] = new DistanceVO();
-	    		distances[i].setHp(rawdata.get(i));
-	    		distances[i].setSum_distance(sum);
-	    		distances[i].setAvg_distance(sum/keys.length);
-	    		System.out.print(distances[i].getAvg_distance()+" ");
+	    		DistanceVO distance = new DistanceVO();
+	    		distance.setHp(rawdata.get(i));
+	    		distance.setSum_distance(sum);
+	    		distance.setAvg_distance(sum/keys.length);
+	    		distances.add(distance);
+	    		System.out.println(distances.get(i).getAvg_distance()+" ");
+
 	    	}
+	    
 	    	
 	    	//정렬
 	    	System.out.println("@@@@@@@@@@@@@@@@@@@@Before sort@@@@@@@@@@@@@@@@@@@@@@@");
 	    	print(distances);
 	    	System.out.println("");
 	    	System.out.println("@@@@@@@@@@@@@@@@@@@@@@After sort@@@@@@@@@@@@@@@@@@@@@@@@");
-	    	Arrays.sort(distances);
+	    	Collections.sort(distances);
 	    	print(distances);
 	    	System.out.println("");
 	    	
@@ -141,25 +178,30 @@ public class RegionSearch {
 	    	DistanceVO[] result = new DistanceVO[3];
 	    	System.out.println("@@@@@@@@@@@@@@@추천 3지역@@@@@@@@@@@@@@@");
 	    	
-	    	
+	    	//i: 결과 반환 배열 인덱스, j: 출발지 인덱스, temp: 거리계산 ArrayList 인덱스
 			int temp = 0;
 	    	for(int i =0; i<3; i++) {
 	    		result[i] = new DistanceVO();
+	    			//입력받은 출발지를 ArrayList에서 찾아 제거합니다.
 	    			for(int j =0; j<keys.length;j++) {
-	    				//입력받은 출발지의 이름과 추천받을 도착지의 이름이 같으면 결과 배열에 넣지 않고 다음 것 탐색
-	    				if(keys[j].equals(distances[temp].getHp().getH_name())) {
-	    					temp++;
+	    				//입력받은 출발지의 이름과 추천받을 도착지의 이름이 같으면 ArrayList에서 제거합니다.
+	    				if(keys[j].equals(distances.get(temp).getHp().getH_name())) {
+	    					distances.remove(distances.get(temp));
+	    				}else {
+	    					temp=0;
 	    				}
 	    			}
-	    			result[i] = distances[temp];
-	    			temp++;
+	    			result[i] = distances.get(temp);
+	    			System.out.println(result[i]);
+	    			distances.remove(distances.get(temp));
+	    			temp=0;
 	    		if(result[2]!=null) {
 	    			break;
 	    		}
 	    	}
 	    	
 	    	
-	    	print(result);
+	    	//print(result);
 	    	ArrayList<HotPlaceVO> result_list = new ArrayList<HotPlaceVO>();
 	    	for(int i=0;i<3;i++) {
 	    		result_list.add(result[i].getHp());
@@ -177,11 +219,12 @@ public class RegionSearch {
 	    private static double rad2deg(double rad) {
 	        return (rad * 180 / Math.PI);
 	    }
-	    //test시 객체 배열을 print해주는 함수입니다.
-	    public static void print(DistanceVO[] distances) {
-			for (int i = 0; i < distances.length; i++) {
-				System.out.println(distances[i]);
+	    //test시 객체 ArrayList을 print해주는 함수입니다.
+	    public static void print(ArrayList<DistanceVO> distances) {
+			for (int i = 0; i < distances.size(); i++) {
+				System.out.println(distances.get(i));
 			}
 		}
 
 }
+
